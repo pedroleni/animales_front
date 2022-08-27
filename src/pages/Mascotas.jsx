3d000337e035e90bae746d0531999e8c7fc587d9
LukmanMartin <lukman.o992@gmail.com>
2022-08-27 16:08:55 +0200

    pag mascota v1

--- src/pages/Mascotas.jsx
+++ src/pages/Mascotas.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
+import { API } from "../services/API";
+
 
 const Mascotas = () => {
-  return (
-    <section className="mascotas">
+  const [allMascotas, setAllMascotas] = useState([]);
+  
+  const getAllMascotas = async () => {
+   API.get("/").then((res) => {
+        setAllMascotas( res);
+          
+         
+   });
+      }
     
-  </section>
-  )
-}
+  
+  useEffect (()=>{
+    getAllMascotas();
+  }, []);
+  
+  return (
+    <section>
+      <h2>Mascotas</h2>
+      <div> {allMascotas.length ? (
+      allMascotas.map ((mascota) => <p>{mascota.name}</p>)
+       ) : (
+         <p>Loading mascotas...</p>
+       )}
+       </div>
+    </section>
+
+  );
+};  
+
 
 export default Mascotas
