dacc54a10672b3b2feac480d288dff6bc8d8bc9c
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-08-27 17:56:45 +0200

    gallery - cardMascotas OK

--- src/components/AnimalCard.jsx
+++ src/components/AnimalCard.jsx
@@ -1,9 +1,25 @@
 
+import "./AnimalCard.css";
+import Swal from "sweetalert2";
+import Mascotas from "../pages/Mascotas";
 
-const AnimalCard = () => {
+const AnimalCard = ({ mascota }) => {
+  
   return (
-    <div>AnimalCard</div>
-  )
-}
+    <figure className="Mascotacard">
+      <img className="card_image" src={mascota.image} alt={mascota.nick} />
+      <h4 className="name_mascota ">{mascota.name}</h4>
+      <h4 className="nick"><span>Nick: </span>{mascota.nick}</h4>
+      <p><span>Residencia: </span>{mascota.location}</p>
+      <p><span>Edad: </span> {mascota.age}</p>
+      
+      {mascota.searchCouple ? (
+        <p className="single">Solter@</p>
+      ) : (
+        <p className="inlOVE">Enamorad@</p>
+      )}
+    </figure>
+  );
+};
 
-export default AnimalCard
+export default AnimalCard;
--- src/pages/Mascotas.jsx
+++ src/pages/Mascotas.jsx
@@ -1,36 +1,43 @@
 import { useEffect, useState } from "react";
 import { API } from "../services/API";
-
+import AnimalCard from "../components/AnimalCard";
+import "./Mascotas.css"
 
 const Mascotas = () => {
   const [allMascotas, setAllMascotas] = useState([]);
-  
+
   const getAllMascotas = async () => {
-   API.get("/").then((res) => {
-        setAllMascotas( res);
-          
-         
-   });
-      }
-    
-  
-  useEffect (()=>{
+    API.get("/").then((res) => {
+      setAllMascotas(res.data.data.animal);
+    });
+  };
+
+  useEffect(() => {
     getAllMascotas();
   }, []);
-  
+
   return (
     <section>
-      <h2>Mascotas</h2>
-      <div> {allMascotas.length ? (
-      allMascotas.map ((mascota) => <p>{mascota.name}</p>)
-       ) : (
-         <p>Loading mascotas...</p>
-       )}
-       </div>
+      <div className="container_mascotas">
+        <h2>Mascotas</h2>
+        <div className="gallery_mascotas">
+          
+            {" "}
+            {allMascotas.length ? (
+              allMascotas.map((mascota) => (
+                <AnimalCard mascota={mascota} key={mascota._id} />
+              ))
+            ) : (
+              <p>Loading mascotas...</p>
+            )}
+        </div>
+
+        <div className="image_footer">
+          <img src="./images/animales.png" alt="logo" />
+        </div>
+      </div>
     </section>
-
   );
-};  
+};
 
-
-export default Mascotas
+export default Mascotas;
